e9deda6bc55990bed23e326dc0566d125b27a2dc
Administrator <Administrator@tenfeePC>
2016-09-01 23:21:46 +0800

    ADD gitignore

--- sourcecodeofsia3part23/Spitter/spitter-web/src/main/java/com/habuma/spitter/mvc/AdminController.java
+++ sourcecodeofsia3part23/Spitter/spitter-web/src/main/java/com/habuma/spitter/mvc/AdminController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
   @RequestMapping("/admin")
-  public void showAdminPage() {
+  public void showAdminPage() { 
   }
 }
